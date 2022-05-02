9c147e74933034889a345b5bb015770f1c9b4807
Preellis <prestone03@gmail.com>
2022-05-01 20:58:37 -0700

    Added Subtractor and Failed Test

---
+++ Subtractor.java
@@ -0,0 +1,5 @@
+public class Subtractor{
+  public static int subtract(int x, int y) {
+    return x-y;
+  }
+}
---
+++ SubtractorTester.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+public class SubtractorTester {
+  @Test
+  public void subtractTest(){
+    assertEquals(0, Subtractor.subtract(1, 2));
+  }
+}
